4502b619cddd2208a54ba97436ec4df8df9cdab6
Tang Juyin <tang.juyin6@gmail.com>
2026-02-24 21:47:33 +0200

    .

--- Assignment08_Template/src/Main.java
+++ Assignment08_Template/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Create and play the game with 2 players
         Game game = new GuessNumberGame();
-        game.play(2);  // The template method runs everything!
+        game.play(2);
     }
 }
